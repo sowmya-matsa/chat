fc2adea0e77cea05a947c5590c835290740fe0b4
sowmya.matsa <sowmya.matsa@gmail.com>
2021-06-07 16:08:51 +0530

    new insertions

--- chatting/admin.py
+++ chatting/admin.py
@@ -1,18 +1,22 @@
 from django.contrib import admin
-from .models import CustomUser, Settings, IndividualChat, GroupChat, Call, Status
+from .models import CustomUser, Settings, IndividualChat, GroupChat, Call, Status, Profile
 
 
 # Register your models here.
 class SettingsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'profile', 'theme']
+
+
+class ProfileAdmin(admin.ModelAdmin):
     list_display = ['id', 'profile_pic', 'name', 'about', 'mobile_no']
 
 
 class IndividualChatAdmin(admin.ModelAdmin):
-    list_display = ['id', 'mobile_no']
+    list_display = ['id', 'name', 'mobile_no']
 
 
 class GroupChatAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'group_image', 'add_user']
+    list_display = ['id', 'name', 'group_image', 'members', 'admin', 'description']
 
 
 class StatusAdmin(admin.ModelAdmin):
@@ -25,6 +29,7 @@ class CallAdmin(admin.ModelAdmin):
 
 admin.site.register(CustomUser)
 admin.site.register(Settings, SettingsAdmin)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(IndividualChat, IndividualChatAdmin)
 admin.site.register(GroupChat, GroupChatAdmin)
 admin.site.register(Call, CallAdmin)
--- chatting/models.py
+++ chatting/models.py
@@ -11,39 +11,51 @@ class CustomUser(AbstractUser):
         return str(self.mobile)
 
 
-class Settings(models.Model):
-    profile_pic = models.ImageField(null=True, blank=True)
-    name = models.CharField(max_length=255)
-    about = models.CharField(max_length=255)
+class Profile(models.Model):
+    profile_pic = models.ImageField(null=True, blank=True, default=None)
+    name = models.CharField(max_length=255, default="")
+    about = models.CharField(max_length=255, default="")
     mobile_no = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, default=None)
-
 
     def __str__(self):
         return str(self.mobile_no)
+
+    class Meta:
+        verbose_name_plural = "profile"
+
+
+class Settings(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    theme = models.CharField(max_length=255, default="")
 
     class Meta:
         verbose_name_plural = "settings"
 
 
 class IndividualChat(models.Model):
+    name = models.CharField(max_length=255,default="")
     mobile_no = models.IntegerField()
 
 
 class GroupChat(models.Model):
     name = models.CharField(max_length=255)
     group_image = models.ImageField(null=True, blank=True)
-    add_user = models.ForeignKey(Settings, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    members = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     chat = models.TextField()
+    admin = models.BooleanField(default=True)
+    description = models.TextField(default="")
 
 
 class Status(models.Model):
     image = models.ImageField(null=True, blank=True)
     caption = models.CharField(max_length=255)
-    contact = models.ForeignKey(Settings, on_delete=models.SET_NULL, null=True, blank=True, default=None)
-    time= models.TimeField()
+    contact = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    time = models.TimeField()
 
     class Meta:
         verbose_name_plural = "Status"
+
+
 class Call(models.Model):
-    contact = models.ForeignKey(Settings, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    contact = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     type_of_call = models.CharField(max_length=255)
