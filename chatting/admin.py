5161a80f0d7eb20fbcf6e13ab1dd7ffdbec93ab5
sowmya.matsa <sowmya.matsa@gmail.com>
2021-06-08 18:00:33 +0530

    updated code

--- chatting/admin.py
+++ chatting/admin.py
@@ -1,30 +1,31 @@
 from django.contrib import admin
-from .models import CustomUser, Settings, IndividualChat, GroupChat, Call, Status, Profile, Chat
+from .models import CustomUser, Settings, IndividualChat, GroupChat, Call, Status, Chat, Contacts
 
 
 # Register your models here.
 class SettingsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'profile', 'theme', 'privacy']
+    list_display = ['id', 'profile_pic', 'name', 'about', 'mobile_no', 'theme', 'last_seen', 'mobile_no']
 
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['id', 'profile_pic', 'name', 'about', 'mobile_no']
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'number']
 
 
 class IndividualChatAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'mobile_no','message']
+    list_display = ['id', 'contact_name', 'message', 'state']
 
 
 class GroupChatAdmin(admin.ModelAdmin):
-    list_display = ['id', 'group_name', 'group_image', 'members', 'admin', 'description','group_chat']
+    list_display = ['id', 'group_name', 'group_image', 'members', 'admin', 'description', 'group_chat', 'creator_name',
+                    'created_date']
 
 
 class ChatAdmin(admin.ModelAdmin):
-    list_display = ['id', 'individual_chart', 'group_chat']
+    list_display = ['id', 'individual_chat', 'group_chat']
 
 
 class StatusAdmin(admin.ModelAdmin):
-    list_display = ['id', 'image', 'caption', 'contact', 'time', 'status_privacy']
+    list_display = ['id', 'image', 'caption', 'time', 'status_privacy']
 
 
 class CallAdmin(admin.ModelAdmin):
@@ -33,7 +34,7 @@ class CallAdmin(admin.ModelAdmin):
 
 admin.site.register(CustomUser)
 admin.site.register(Settings, SettingsAdmin)
-admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Contacts, ContactsAdmin)
 admin.site.register(IndividualChat, IndividualChatAdmin)
 admin.site.register(GroupChat, GroupChatAdmin)
 admin.site.register(Chat, ChatAdmin)
--- chatting/models.py
+++ chatting/models.py
@@ -11,65 +11,68 @@ class CustomUser(AbstractUser):
         return str(self.mobile)
 
 
-class Profile(models.Model):
+class Contacts(models.Model):
+    name = models.CharField(max_length=255, default="")
+    number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = "contacts"
+
+
+class Settings(models.Model):
     profile_pic = models.ImageField(null=True, blank=True, default=None)
     name = models.CharField(max_length=255, default="")
     about = models.CharField(max_length=255, default="")
     mobile_no = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, default=None)
-
-    def __str__(self):
-        return str(self.mobile_no)
-
-    class Meta:
-        verbose_name_plural = "profile"
-
-
-class Settings(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     theme = models.CharField(max_length=255, default="")
-    privacy=models.BooleanField(default=True)
+    last_seen = models.CharField(max_length=255, default="")
 
     class Meta:
         verbose_name_plural = "settings"
 
 
 class IndividualChat(models.Model):
-    name = models.CharField(max_length=255, default="")
-    mobile_no = models.IntegerField()
+    contact_name = models.ForeignKey(Contacts, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     message = models.CharField(max_length=255, default="")
+    state = models.CharField(max_length=255, default="")
 
     def __str__(self):
-        return str(self.name) + "," + str(self.message)
+        return str(self.contact_name) + "," + str(self.message)
 
 
 class GroupChat(models.Model):
     group_name = models.CharField(max_length=255)
     group_image = models.ImageField(null=True, blank=True)
-    members = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    members = models.ForeignKey(Contacts, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     group_chat = models.TextField()
     admin = models.BooleanField(default=True)
     description = models.TextField(default="")
+    creator_name = models.CharField(max_length=255, default="")
+    created_date = models.DateField()
 
     def __str__(self):
         return str(self.group_name) + "," + str(self.group_chat)
 
 
 class Chat(models.Model):
-    individual_chart = models.ForeignKey(IndividualChat, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    individual_chat = models.ForeignKey(IndividualChat, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     group_chat = models.ForeignKey(GroupChat, on_delete=models.SET_NULL, null=True, blank=True, default=None)
 
 
 class Status(models.Model):
     image = models.ImageField(null=True, blank=True)
     caption = models.CharField(max_length=255)
-    contact = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     time = models.TimeField()
-    status_privacy= models.CharField(max_length=255,default="")
+    status_privacy = models.CharField(max_length=255, default="")
+
     class Meta:
         verbose_name_plural = "Status"
 
 
 class Call(models.Model):
-    contact = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, default=None)
-    mode_of_call = models.CharField(max_length=255,default="")
-    type_of_call= models.CharField(max_length=255,default="")
+    contact = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    mode_of_call = models.CharField(max_length=255, default="")
+    type_of_call = models.CharField(max_length=255, default="")
